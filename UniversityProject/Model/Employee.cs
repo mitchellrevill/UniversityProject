6d457aeaeee9100ca601d7e349474f8e8223f7a6
mitch <mitch@MITCH>
2024-10-15 18:38:13 +0100

    awdsad

---
+++ UniversityProject/Model/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityProject.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Role { get; set; }
+        public string Location { get; set; }
+        public string Department { get; set; }
+        public EmployeeStatus Status { get; set; }
+
+        public enum EmployeeStatus
+        {
+            FullTime,
+            PartTime,
+            Contract
+        }
+    }
+}
---
+++ UniversityProject/Model/EmployeeTimeSheet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityProject.Model
+{
+    public class TimeSheet
+    {
+        public int Id { get; set; }
+        public Employee Employee { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TimeSpan BreakTime { get; set; }
+        public TimeSpan TotalHoursWorked { get => EndTime - StartTime - BreakTime; }
+
+    }
+}
