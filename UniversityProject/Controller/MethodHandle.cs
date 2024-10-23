c298403fc09e3a28709b3a7366fd7f6433191ec3
mitch <mitch@MITCH>
2024-10-23 19:34:16 +0100

    Merging changes i forget to change

--- UniversityProject/Controller/MethodHandle.cs
+++ UniversityProject/Controller/MethodHandle.cs
@@ -2,6 +2,7 @@
 using System;
 using System.IO;
 using System.Net;
+using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Tasks;
 using UniversityProject.Data;
@@ -11,6 +12,7 @@ namespace HRSystem
 {
     public static class MethodHandle
     {
+        // Links to methods here, will handle requests, if request not sent will got to serve static file 
         public static async Task HandleRequest(HttpListenerRequest req, HttpListenerResponse resp)
         {
             string requestedPath = req.Url.AbsolutePath.TrimStart('/');
@@ -21,8 +23,16 @@ namespace HRSystem
             }
             else if (requestedPath == "GetAllEmployees" && req.HttpMethod == "GET")
             {
+                Console.WriteLine("Reached GetAllEmployee method Start");
+                await GetEmployees(req, resp);
+            }
+            else if (requestedPath == "UpdateEmployees" && req.HttpMethod == "POST")
+            {
                 Console.WriteLine("Reached method Start");
-                await GetAllEmployees(req, resp);
+                await UpdateEmployee(req, resp);
+            } else if (requestedPath == "DeleteEmployee" && req.HttpMethod == "POST")
+            {
+                DeleteEmployee(req, resp);
             }
             else
             {
@@ -31,27 +41,22 @@ namespace HRSystem
         }
 
 
-        private static async Task GetAllEmployees(HttpListenerRequest req, HttpListenerResponse resp)
+        private static async Task GetEmployees(HttpListenerRequest req, HttpListenerResponse resp)
         {
             string dbPath = Path.Combine(HttpServer.ResourcesDirectory, "database.db");
             var employeeDb = new EmployeeDatabase(dbPath);
+            //GETS DATA FROM SQL
             var employees = employeeDb.GetAllEmployees();
-
+            //TURNS DATA FROM C# OBject to JSON
             string jsonResponse = JsonConvert.SerializeObject(employees);
 
+           // REPLIES BACK TO WEBSITE
             byte[] data = Encoding.UTF8.GetBytes(jsonResponse);
             resp.ContentType = "application/json";
             resp.ContentLength64 = data.Length;
             await resp.OutputStream.WriteAsync(data, 0, data.Length);
             Console.WriteLine("Reached method end");
         }
-        private static async Task InsertEmployee(HttpListenerRequest req, HttpListenerResponse resp)
-        {
-            using (var reader = new StreamReader(req.InputStream, req.ContentEncoding))
-            {
-                string json = await reader.ReadToEndAsync();
-                var newEmployee = JsonConvert.DeserializeObject<Employee>(json);
-
 
 
         private static async Task InsertEmployee(HttpListenerRequest req, HttpListenerResponse resp)
@@ -72,6 +77,59 @@ namespace HRSystem
             }
         }
 
+        // Same as insertemployee, used inpart of method to update if current employeeId exists 
+        private static async Task UpdateEmployee(HttpListenerRequest req, HttpListenerResponse resp)
+        {
+            using (var reader = new StreamReader(req.InputStream, req.ContentEncoding))
+            {
+                string json = await reader.ReadToEndAsync();
+                var newEmployee = JsonConvert.DeserializeObject<Employee>(json);
+
+                string dbPath = Path.Combine(HttpServer.ResourcesDirectory, "database.db");
+                var employeeDb = new EmployeeDatabase(dbPath);
+                employeeDb.InsertEmployee(newEmployee);
+
+                byte[] data = Encoding.UTF8.GetBytes("Employee inserted successfully.");
+                resp.ContentType = "text/plain";
+                resp.ContentLength64 = data.Length;
+                await resp.OutputStream.WriteAsync(data, 0, data.Length);
+            }
+        }
+
+
+        // can use employee class to insert only employeeId if needed, no need to pass more than employeeId
+        private static async Task DeleteEmployee(HttpListenerRequest req, HttpListenerResponse resp)
+        {
+            using (var reader = new StreamReader(req.InputStream, req.ContentEncoding))
+            {
+                string json = await reader.ReadToEndAsync();
+                var Employee = JsonConvert.DeserializeObject<Employee>(json);
+                string dbPath = Path.Combine(HttpServer.ResourcesDirectory, "database.db");
+                var employeeDb = new EmployeeDatabase(dbPath);
+                employeeDb.DeleteEmployee(Employee.EmployeeId);
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Static dont touch 
         private static async Task ServeStaticFile(HttpListenerRequest req, HttpListenerResponse resp, string requestedPath)
         {
             string filePath = Path.Combine(HttpServer.ResourcesDirectory, requestedPath);
